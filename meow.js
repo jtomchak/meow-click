5b70d42444129c86f44cff51d7292cfd1a12e335
Jesse Tomchak <jtomchak@gmail.com>
2017-10-27 16:27:38 -0700

    winner winner chx dinner

--- meow.js
+++ meow.js
@@ -2,24 +2,30 @@ window.onload = function() {
   console.log("READY!!! MEOW!!!");
   //Get button element
   var searchGiphyButton = document.getElementById("getMeow");
+  var giphyImage = document.getElementById("rando-gliph-img");
+  var notYetText = document.getElementById("nothing-yet");
+  var searchInput = document.getElementById("textInput");
 
   searchGiphyButton.addEventListener("click", function() {
     //go get kitten gliph's
     console.log("Meow Click Click!!!");
-    fetchGiphy();
+    fetchGiphy(searchInput.value);
   });
 
-  function fetchGiphy() {
+  function fetchGiphy(str) {
     var xhr = new XMLHttpRequest();
     //open
     xhr.open(
       "GET",
-      "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=kitten"
+      "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + str
     );
 
     xhr.onload = function() {
       if (xhr.status === 200 && xhr.readyState === 4) {
-        console.log(xhr.response);
+        var payload = JSON.parse(xhr.response);
+        console.log(payload.data.image_url);
+        giphyImage.src = payload.data.image_url;
+        notYetText.setAttribute("class", "nothing-yet-hidden");
       }
     };
 
